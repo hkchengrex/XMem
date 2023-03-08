346c078d51e52d3f21bcfc5d6860bf8c64d9f9dc
Lin Han <me@linhan.email>
2023-03-08 03:39:37 +0000

    Change np.float to np.float32

--- inference/interact/fbrs/inference/predictors/brs_functors.py
+++ inference/interact/fbrs/inference/predictors/brs_functors.py
@@ -72,7 +72,7 @@ class BaseOptimizer:
         self._last_mask = current_mask
 
         loss.backward()
-        f_grad = opt_params.grad.cpu().numpy().ravel().astype(np.float)
+        f_grad = opt_params.grad.cpu().numpy().ravel().astype(np.float32)
 
         return [f_val, f_grad]
 
